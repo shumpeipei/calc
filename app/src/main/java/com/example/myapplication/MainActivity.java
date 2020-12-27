2eb62bf1b78f476ccad7be702bac3ec49173c284
Shumpei Koike <sss.seed@gmail.com>
2020-12-27 21:47:05 +0900

    ver1.0

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,22 +16,102 @@ public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button button;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.textview);
-        final EditText editText = (EditText) findViewById(R.id.edittext);
+        textView = (TextView) findViewById(R.id.textview);
+        editText = (EditText) findViewById(R.id.edittext);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(buttonListener);
 
+        findViewById(R.id.button_1).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_2).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_3).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_4).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_5).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_6).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_7).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_8).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_9).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_0).setOnClickListener(buttonNumberListener);
+        findViewById(R.id.button_dot).setOnClickListener(buttonNumberListener);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textView.setText(editText.getText().toString());
-            }
-        });
-
+        findViewById(R.id.button_add).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.button_subtract).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.button_multiply).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.button_divide).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.button_equal).setOnClickListener(buttonOperatorListener);
+        findViewById(R.id.button_c).setOnClickListener(buttonListener);
     }
+
+    int recentOperator = R.id.button_equal; // 最近押された計算キー
+    double result;  // 計算結果
+    boolean isOperatorKeyPushed;    // 計算キーが押されたことを記憶
+
+    View.OnClickListener buttonOperatorListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button operatorButton = (Button) view;
+            double value = Double.parseDouble(editText.getText().toString());
+            if (recentOperator == R.id.button_equal) {
+                result = value;
+            } else {
+                result = calc(recentOperator, result, value);
+
+            }
+
+            recentOperator = operatorButton.getId();
+            textView.setText(operatorButton.getText());
+            editText.setText(String.valueOf(result));
+            isOperatorKeyPushed = true;
+        }
+    };
+
+
+
+    double calc(int operator, double value1, double value2) {
+        switch (operator) {
+            case R.id.button_add:
+                return value1 + value2;
+            case R.id.button_subtract:
+                return value1 - value2;
+            case R.id.button_multiply:
+                return value1 * value2;
+            case R.id.button_divide:
+                return value1 / value2;
+            default:
+                return value1;
+        }
+    }
+
+
+    View.OnClickListener buttonNumberListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button button = (Button) view;
+
+            if (isOperatorKeyPushed == true) {
+                editText.setText(button.getText());
+            } else {
+                editText.append(button.getText());
+            }
+
+            isOperatorKeyPushed = false;
+        }
+    };
+
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            recentOperator = R.id.button_equal;
+            result = 0;
+            isOperatorKeyPushed = false;
+
+            textView.setText("");
+            editText.setText("");
+        }
+    };
 }
